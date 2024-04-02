19b954836580d75d09a5d139ddd9e4e6f1cdb838
BaristaBarita <18020776@usc.edu.ph>
2024-04-02 10:14:48 +0800

    created sample assets data to be used

---
+++ client/src/util/data/sampleAssetsData.js
@@ -0,0 +1,58 @@
+export const truckerAssets = [
+    {
+        id: 1,
+        asset_type: "Truck",
+        truck_name: "Roller",
+        brand: "Volvo",
+        truck_type: "Flatbed",
+        plate_number: "ABC123"
+    },
+    {
+        id: 2,
+        asset_type: "Trailer",
+        measurement: "40'",
+        trailer_type: "Enclosed",
+        plate_number: "XYZ789"
+    },
+    {
+        id: 3,
+        asset_type: "Truck",
+        truck_name: "Boi",
+        brand: "Mercedes",
+        truck_type: "Refrigerated",
+        plate_number: "DEF456"
+    },
+    {
+        id: 4, 
+        asset_type: "Trailer",
+        measurement: "20'",
+        trailer_type: "Open Top",
+        weight: "100.00 kg",
+        plate_number: "UVW123"
+    },
+    {
+        id:5, 
+        asset_type: "Truck",
+        truck_name: "Musk",
+        brand: "Tesla",
+        truck_type: "Electric",
+        plate_number: "GHI789"
+    },
+    {
+        id:6,
+        asset_type: "Truck",
+        truck_name: "Harrison",
+        brand: "Ford",
+        truck_type: "Cargo",
+        plate_number: "AZR-567J"
+    },
+    {
+        id: 7,
+        asset_type: "Trailer",
+        measurement: "40'",
+        weight: "100.00 kg",
+        trailer_type: "Enclosed",
+        plate_number: "FLD-890K"
+    },
+
+]
