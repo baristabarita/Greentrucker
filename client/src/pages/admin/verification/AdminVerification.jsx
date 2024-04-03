314c04f5f601ba9a889853090bef35452364571f
BaristaBarita <18020776@usc.edu.ph>
2024-04-04 02:55:28 +0800

    created admin vverifications page

--- client/src/pages/admin/verification/AdminVerification.jsx
+++ client/src/pages/admin/verification/AdminVerification.jsx
@@ -1,10 +1,145 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { verifyRequests } from "@/util/data/sampleVerificationsData";
+import { BsCheckSquareFill } from "react-icons/bs";
 
 const AdminVerification = () => {
+
+    const navigate = useNavigate();
+    const [requests, setRequests] = useState(verifyRequests);
+    const [currentPage, setCurrentPage] = useState(1);
+    const requestsPerPage = 10;
+    const [filterStatus, setFilterStatus] = useState('');
+
+    const statusOptions = ["All", "Pending", "Verified", "Declined"];
+
+    const getFilteredRequests = () => {
+        return requests.filter(request => {
+            const statusMatches = filterStatus === "All" || filterStatus === '' ? true : request.status === filterStatus;
+            return statusMatches;
+        });
+    };
+
+    const totalPages = Math.ceil(getFilteredRequests().length / requestsPerPage);
+
+    const indexOfLastRequest = currentPage * requestsPerPage;
+    const indexOfFirstRequest = indexOfLastRequest - requestsPerPage;
+    const currentRequests = getFilteredRequests().slice(indexOfFirstRequest, indexOfLastRequest);
+
+    // Function to navigate to the next and previous page
+    const goToNextPage = () => setCurrentPage((prev) => (prev < totalPages ? prev + 1 : prev));
+    const goToPreviousPage = () => setCurrentPage((prev) => (prev > 1 ? prev - 1 : prev));
+
+    // Function to determine if the next or previous buttons should be disabled
+    const nextButtonDisabled = currentPage >= totalPages;
+    const prevButtonDisabled = currentPage <= 1;
+
+    const handleViewDetails = (requestId) => {
+        alert("deets")
+    };
     return (
-        <>
-            <h1>AdminVerification</h1>
-        </>
+        <div className="animate-fade-in p-5">
+            <div className="flex items-center mb-4 text-primarycolor">
+                <BsCheckSquareFill className="text-3xl mr-2" />
+                <h2 className="text-3xl font-bold">Account Verification Requests</h2>
+            </div>
+            {/* Filter */}
+            <div className="flex justify-between items-center">
+                <div className="flex items-center space-x-3 bg-useradmin rounded-lg p-3 shadow-sm">
+                    <span className="font-medium text-primarycolor">Filter by:</span>
+                    <div>
+                        <select
+                            value={filterStatus}
+                            onChange={(e) => setFilterStatus(e.target.value)}
+                            className="select select-bordered select-sm bg-useradmin text-secondarycolor"
+                        >
+                            {statusOptions.map((status) => (
+                                <option key={status} value={status}>{status}</option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            {/* Table to display requests */}
+            <div className="overflow-y-auto mt-4 bg-useradmin rounded-lg drop-shadow-lg shadow-lg opacity-1">
+                <table className="w-full text-sm text-center text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-primarycolor">
+                        <tr>
+                            <th scope="col" className="py-3 px-6">Company Name</th>
+                            <th scope="col" className="py-3 px-6">Owner Name</th>
+                            <th scope="col" className="py-3 px-6">Email Address</th>
+                            <th scope="col" className="py-3 px-6">Request Date</th>
+                            <th scope="col" className="py-3 px-6">Status</th>
+                            <th scope="col" className="py-3 px-6">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {currentRequests.length > 0 ? (
+                            currentRequests.map((request) => (
+                                <tr key={request.id} className="bg-useradmin border-b">
+                                    <td className="py-4 px-6 text-primarycolor">{request.company_name}</td>
+                                    <td className="py-4 px-6 text-primarycolor">{request.contact_name}</td>
+                                    <td className="py-4 px-6 text-primarycolor">{request.email_address}</td>
+                                    <td className="py-4 px-6 text-primarycolor">{request.sent_date}</td>
+                                    <td className="py-4 px-6 text-center">
+                                        <div className="flex justify-center items-center">
+                                            <span className={`inline-flex items-center justify-center font-bold rounded-full py-1 px-3 text-sm 
+                                                ${request.status === "Pending" ? "bg-pending bg-opacity-30 text-orange-400" :
+                                                    request.status === "Verified" ? "bg-complete bg-opacity-30 text-green-400" :
+                                                        request.status === "Declined" ? "bg-cancelled bg-opacity-30 text-red-400" :
+                                                            "bg-gray-100 text-gray-800"}`}>
+                                                {request.status}
+                                            </span>
+                                        </div>
+                                    </td>
+                                    <td className="py-4 px-6 flex justify-center items-center">
+                                        <button className="font-bold px-4 py-1 rounded-lg bg-secondarycolor text-userclient hover:bg-primarycolor hover:underline mr-2"
+                                            onClick={() => handleViewDetails(request.id)}
+
+                                        >View Details
+                                        </button>
+
+                                    </td>
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td colSpan="6" className="text-center py-4">No Requests right now</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+
+            {/* Pagination and Showing X of Y requests */}
+            <div className="flex justify-between items-center mt-4">
+                <span className="text-primarycolor font-bold">Showing {currentRequests.length} of {requests.length} Verification Requests</span>
+                <div className="flex">
+                    <button
+                        onClick={goToPreviousPage}
+                        disabled={prevButtonDisabled}
+                        className={`px-4 py-2 bg-useradmin text-secondarycolor font-semibold rounded-md border border-gray-400 hover:bg-gray-300 ${prevButtonDisabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+                    >
+                        &#8592;
+                    </button>
+
+                    <div className="flex items-center justify-center">
+                        <span className="px-4 py-2 bg-useradmin text-primarycolor font-semibold rounded-md">
+                            Page {currentPage} of {totalPages}
+                        </span>
+                    </div>
+
+                    <button
+                        onClick={goToNextPage}
+                        disabled={nextButtonDisabled}
+                        className={`px-4 py-2 bg-useradmin text-secondarycolor font-semibold rounded-md border border-gray-400 hover:bg-gray-300 ${nextButtonDisabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+                    >
+                        &#8594;
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
 
---
+++ client/src/util/data/sampleVerificationsData.js
@@ -0,0 +1,92 @@
+export const verifyRequests = [
+    { 
+        id: 1, 
+        contact_name: 'John Doe', 
+        company_name: 'Doe Trucking', 
+        email_address: 'doe@gmail.com',
+        address: '1234 Main St, City, State',
+        status: "Pending", 
+        sent_date: '2024-04-04' 
+    },
+    { 
+        id: 2, 
+        contact_name: 'Kyle Garrick', 
+        company_name: 'KG Trucking', 
+        email_address: 'garrick@gmail.com',
+        address: '5678 Market St, New City, State',
+        status: "Pending",  
+        sent_date: '2024-04-02' 
+    },
+    { 
+        id: 3, 
+        contact_name: 'Simon Riley', 
+        company_name: 'SR Trucking', 
+        email_address: 'simon@gmail.com',
+        address: '1234 Maple Street, Springfield, IL', 
+        status: "Pending", 
+        sent_date: '2024-03-31' 
+    },
+    {
+        id: 4,
+        contact_name: 'Alex Mercer',
+        company_name: 'Mercer Transport',
+        email_address: 'alex.mercer@example.com',
+        address: '4567 Pine St, Liberty, NY',
+        status: "Verified",
+        sent_date: '2024-03-28'
+    },
+    {
+        id: 5,
+        contact_name: 'Helen Parr',
+        company_name: 'Parr Logistics',
+        email_address: 'helen.parr@example.com',
+        address: '7890 Elm St, Metroville, CA',
+        status: "Declined",
+        sent_date: '2024-03-27'
+    },
+    {
+        id: 6,
+        contact_name: 'Peter Parker',
+        company_name: 'Parker Express',
+        email_address: 'peter.parker@example.com',
+        address: '1357 Broadway St, Queens, NY',
+        status: "Verified",
+        sent_date: '2024-03-25'
+    },
+    {
+        id: 7,
+        contact_name: 'Bruce Wayne',
+        company_name: 'Wayne Enterprises',
+        email_address: 'bruce.wayne@example.com',
+        address: '2468 Gotham St, Gotham, NJ',
+        status: "Pending",
+        sent_date: '2024-03-22'
+    },
+    {
+        id: 8,
+        contact_name: 'Clark Kent',
+        company_name: 'Kent Farming',
+        email_address: 'clark.kent@example.com',
+        address: '9753 Smallville St, Smallville, KS',
+        status: "Verified",
+        sent_date: '2024-03-20'
+    },
+    {
+        id: 9,
+        contact_name: 'Diana Prince',
+        company_name: 'Themyscira Antiques',
+        email_address: 'diana.prince@example.com',
+        address: '8642 Amazon St, Gateway City, CA',
+        status: "Declined",
+        sent_date: '2024-03-18'
+    },
+    {
+        id: 10,
+        contact_name: 'Tony Stark',
+        company_name: 'Stark Industries',
+        email_address: 'tony.stark@example.com',
+        address: '7531 Malibu Point, Malibu, CA',
+        status: "Verified",
+        sent_date: '2024-03-15'
+    }
+];
