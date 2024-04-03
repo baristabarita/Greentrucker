d234222fd23c1d5570454c0456eaee51316c5f72
BaristaBarita <18020776@usc.edu.ph>
2024-04-03 12:09:29 +0800

    modified trucker sidebar to properly logout

--- client/src/components/sidebar/TruckerSidebar.jsx
+++ client/src/components/sidebar/TruckerSidebar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import useAuth from '@/util/userAuthentication';
 import { FaBook, FaTruck, FaDollarSign, FaUser, FaSignOutAlt } from 'react-icons/fa';
 import { BiSolidDashboard } from 'react-icons/bi';
 
 const TruckerSide = ({ isSidebarOpen, toggleSidebar }) => {
+  const { isLoggedIn, userType, logout } = useAuth();
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -13,14 +15,19 @@ const TruckerSide = ({ isSidebarOpen, toggleSidebar }) => {
       toggleSidebar(); // Close sidebar when navigating if it's not open
     }
   };
-
+  const handleLogout = () => {
+    logout();
+    window.location.reload(); // Reloads the page after logout
+  };
+  console.log("LOG STATUS: ", isLoggedIn);
+  console.log("USERTYPE: ", userType);
   return (
     <div className={`h-screen fixed bg-usertrucker text-[#B1B1B1] text-[1.2em]  font-semibold p-4 flex flex-col items-start ${isSidebarOpen ? 'w-60' : 'w-16'} transition-all duration-300 ease-in-out`}>
       <div className={`mb-5 cursor-pointer flex items-center ${location.pathname === '/trucker/truckerdash' ? ' text-primarycolor rounded' : 'hover:text-primarycolor'}`} onClick={() => handleNavigation('/trucker/truckerdash')}>
         <BiSolidDashboard className={`mr-2`} />
         {isSidebarOpen && 'Dashboard'}
       </div>
-      <div className={`mb-5 cursor-pointer flex items-center ${location.pathname === '/trucker/truckerbookings' || location.pathname === '/trucker/truckerbookings/booking' || location.pathname === '/trucker/truckerbookings/bookingform'  ? ' text-primarycolor rounded' : 'hover:text-primarycolor'}`} onClick={() => handleNavigation('/trucker/truckerbookings')}>
+      <div className={`mb-5 cursor-pointer flex items-center ${location.pathname === '/trucker/truckerbookings' || location.pathname === '/trucker/truckerbookings/booking' || location.pathname === '/trucker/truckerbookings/bookingform' ? ' text-primarycolor rounded' : 'hover:text-primarycolor'}`} onClick={() => handleNavigation('/trucker/truckerbookings')}>
         <FaBook className={`mr-2`} />
         {isSidebarOpen && 'Bookings'}
       </div>
@@ -36,7 +43,7 @@ const TruckerSide = ({ isSidebarOpen, toggleSidebar }) => {
         <FaUser className={`mr-2`} />
         {isSidebarOpen && 'Account Settings'}
       </div>
-      <div className={`fixed bottom-[5%] ml-2 w-[15%] cursor-pointer flex items-center hover:text-primarycolor`} onClick={() => handleNavigation('/')}>
+      <div className={`fixed bottom-[5%] ml-2 w-[15%] cursor-pointer flex items-center hover:text-primarycolor`} onClick={handleLogout}>
         <FaSignOutAlt className={`mr-2 mt-1`} />
         {isSidebarOpen && 'Logout'}
       </div>
